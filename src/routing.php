e0637a95799b474484d21200a916bac222f70868
Jimmy Achour <jimmyachour@hotmail.fr>
2019-04-14 00:05:14 +0200

    Modif rooter pour passage auto de la methode index si parametre vide

--- src/routing.php
+++ src/routing.php
@@ -9,9 +9,10 @@
  * @link     https://github.com/WildCodeSchool/simple-mvc
  */
 
+
 $routeParts = explode('/', ltrim($_SERVER['REQUEST_URI'], '/') ?: HOME_PAGE);
 $controller = 'App\Controller\\' . ucfirst($routeParts[0] ?? '') . 'Controller';
-$method = $routeParts[1] ?? '';
+$method = $routeParts[1] ?? MAIN_PAGE;
 $vars = array_slice($routeParts, 2);
 
 if (class_exists($controller) && method_exists(new $controller(), $method)) {
