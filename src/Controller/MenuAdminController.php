e1779fa075d2b5a28f57343a2c078a8dad6fb15f
Jimmy Achour <jimmyachour@hotmail.fr>
2019-04-17 17:32:08 +0200

    Display dish dirty style

--- src/Controller/MenuAdminController.php
+++ src/Controller/MenuAdminController.php
@@ -3,16 +3,28 @@
 namespace App\Controller;
 
 
+use App\Model\DishManager;
+
 class MenuAdminController extends AbstractController
 {
-    public function menu()
+    public function index()
     {
 
         return $this->twig->render('MenuAdmin/index.html.twig');
     }
 
-    public function list()
+    public function listmenu()
     {
-        return $this->twig->render('MenuAdmin/list.html.twig');
+        return $this->twig->render('MenuAdmin/listmenu.html.twig');
+    }
+
+    public function listdish()
+    {
+        $allDish = new DishManager();
+        $allDish = $allDish->selectAll();
+
+
+
+        return $this->twig->render('MenuAdmin/listdish.html.twig',[ 'allDish' => $allDish]);
     }
 }
