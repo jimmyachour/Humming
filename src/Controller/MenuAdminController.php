582ce2366a257280072976b857a26c4ac8f91b45
Jimmy Achour <jimmyachour@hotmail.fr>
2019-04-23 15:03:36 +0200

    Gestion de carte Delete et lecture ok

--- src/Controller/MenuAdminController.php
+++ src/Controller/MenuAdminController.php
@@ -20,11 +20,49 @@ class MenuAdminController extends AbstractController
 
     public function listdish()
     {
-        $allDish = new DishManager();
-        $allDish = $allDish->selectAll();
+        $allDishManager = new DishManager();
+        $allDish = $allDishManager->selectAll();
 
+        return $this->twig->render('MenuAdmin/listdish.html.twig', [ 'allDish' => $allDish]);
+    }
 
+    public function dish($id)
+    {
+        $dishManager = new DishManager();
+        $dish = $dishManager->selectOneById($id);
 
-        return $this->twig->render('MenuAdmin/listdish.html.twig',[ 'allDish' => $allDish]);
+        return $this->twig->render('MenuAdmin/dishForm.html.twig', ['dish' => $dish]);
+    }
+
+    public function dishDelete($id)
+    {
+        header('Location: /admin/menu/listdish');
+
+        $dishManager = new DishManager();
+        $dishManager->delete($id);
+    }
+
+    public function liststart()
+    {
+        $allDishManager = new DishManager();
+        $allDish = $allDishManager->selectAllByType('start');
+
+        return $this->twig->render('MenuAdmin/listdish.html.twig', [ 'allDish' => $allDish]);
+    }
+
+    public function listbetween()
+    {
+        $allDishManager = new DishManager();
+        $allDish = $allDishManager->selectAllByType('between');
+
+        return $this->twig->render('MenuAdmin/listdish.html.twig', [ 'allDish' => $allDish]);
+    }
+
+    public function listdessert()
+    {
+        $allDishManager = new DishManager();
+        $allDish = $allDishManager->selectAllByType('dessert');
+
+        return $this->twig->render('MenuAdmin/listdish.html.twig', [ 'allDish' => $allDish]);
     }
 }
--- src/Model/DishManager.php
+++ src/Model/DishManager.php
@@ -14,5 +14,19 @@ class DishManager extends AbstractManager
         parent::__construct(self::TABLE);
     }
 
+    /**
+     * Get all row from database.
+     *
+     * @return array
+     */
+    public function selectAllByType($type): array
+    {
+        $statement = $this->pdo->prepare("SELECT * FROM $this->table WHERE type=:type");
+
+        $statement->bindValue('type', $type, \PDO::PARAM_STR);
+        $statement->execute();
+
+        return $statement->fetchAll();
+    }
 
 }
