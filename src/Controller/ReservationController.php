7d2fb8fe7147e2b3a959c761382d4fd401320199
Pascal Monge <oenos@free.fr>
2019-04-25 09:13:28 +0200

    la page reservation

---
+++ src/Controller/ReservationController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\ClientManager;
+use App\Entity\Client;
+use App\Model\ReservationManager;
+
+class ReservationController extends AbstractController
+{
+    public function request()
+    {
+        $notification = null;
+        if(isset($_GET['resa']) && $_GET['resa']=='success'){
+            $notification = "Votre reservation est enregistrée !";
+        }
+        return $this->twig->render('Reservation/index.html.twig', ['notification' => $notification]);
+    }
+
+    public function add()
+    {
+
+        $reservationManager = new ReservationManager();
+
+        $reservationManager->add($_POST);
+
+        header('Location:/reservation/request/?resa=success');
+        exit();
+    }
+
+}
---
+++ src/Model/ReservationManager.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Model;
+
+/**
+ * Class ReservationManager
+ * @package App\Model
+ */
+class ReservationManager extends AbstractManager  // class ClientManager
+{
+
+    /**
+     * ReservationManager constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct('reservation');
+    }
+
+    /**
+     * @param array $reservation
+     * @return int
+     */
+    public function add(array $reservation): int
+    {
+        $sql = "INSERT INTO $this->table (reservDate, reservTime, reservNbr) VALUES (:reservDate, :reservTime, :reservNbr)";
+        $statement = $this->pdo->prepare($sql);
+
+        $statement->bindValue('reservDate', $reservation['reservDate'], \PDO::PARAM_STR);
+        $statement->bindValue('reservTime', $reservation['reservTime'], \PDO::PARAM_STR);
+        $statement->bindValue('reservNbr', $reservation['reservNbr'], \PDO::PARAM_STR);
+
+
+        if ($statement->execute()) {
+            return (int)$this->pdo->lastInsertId();
+        }
+    }
+
+}
+
